145ea14b97ab7c81279164151b22585d08910f4d
Mateus <mateusrubinho26@gmail.com>
2023-05-18 18:00:01 -0400

    móCoisa

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+ 
+//   document.addEventListener('scroll', function(){
+//     const header = document.querySelector('header');
+//         if(window.screenY > 0)
+//         {
+//           //  header.classList.add('scrolled')
+//           header.classList.add('scrolled');
+//         }else
+//         {
+//             header.classList.remove('scrolled')
+//         }
+//       })
